a8af9fd02d027914edff0de2acedfcabdd9fb573
ASW-研修３ <training03@ASW-kenshuu3noMacBook-Air.local>
2020-05-15 13:50:47 +0900

    searchの検索結果画面

---
+++ CosmeApp/Search/Search result/User/UserSearchResultViewController.swift
@@ -0,0 +1,34 @@
+//
+//  UserSearchResultViewController.swift
+//  CosmeApp
+//
+//  Created by ASW-研修３ on 2020/05/15.
+//  Copyright © 2020 ASW-研修２. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+// MARK: - Property
+class UserSearchResultViewController: BaseViewController {
+    @IBOutlet weak var mainView: UserSearchResultMainView!
+}
+// MARK: - Life cycle
+extension UserSearchResultViewController {
+    override func loadView() {
+        super.loadView()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
+// MARK: - Protocol
+extension UserSearchResultViewController {
+}
+// MARK: - method
+extension UserSearchResultViewController {
+}
+
+
