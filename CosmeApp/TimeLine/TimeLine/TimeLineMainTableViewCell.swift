057ad1647eca913bcf2859707b21f04f28b6c4c4
UshigomeSaki <s.ushigome@a-sw.jp>
2020-05-15 16:23:45 +0900

    TimeLineVCの作成

--- CosmeApp/TimeLine/TimeLine/TimeLineMainTableViewCell.swift
+++ CosmeApp/TimeLine/TimeLine/TimeLineMainTableViewCell.swift
@@ -16,16 +16,37 @@ extension TimeLineMainTableViewCellDelegate {
 // MARK: - Property
 class TimeLineMainTableViewCell: BaseTableViewCell {
     weak var delegate: TimeLineMainTableViewCellDelegate? = nil
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionViewFlowLayout: UICollectionViewFlowLayout!
 }
 // MARK: - Life cycle
 extension TimeLineMainTableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
+        setDelegate()
+        setLayout()
+        
+        loadCollectionViewCellFromXib(collectionView: collectionView, cellName: "TimeLineMainCollectionViewCell")
     }
 }
 // MARK: - Protocol
-extension TimeLineMainTableViewCell {
+extension TimeLineMainTableViewCell :UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TimeLineMainCollectionViewCell", for: indexPath) as? TimeLineMainCollectionViewCell else {return UICollectionViewCell()}
+        return cell
+    }
+    
 }
 // MARK: - method
 extension TimeLineMainTableViewCell {
+    func setDelegate(){
+        collectionView.dataSource = self
+    }
+    func setLayout(){
+        collectionViewFlowLayout.estimatedItemSize = CGSize(width:80,height:80)
+    }
 }
