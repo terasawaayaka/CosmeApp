1091077929441e2648157bae234940f580231105
norimatsushuusei <s.norimatsu@a-sw.jp>
2020-05-15 12:00:19 +0900

    fix profile

--- CosmeApp/Profile/MyProfileViewController.swift
+++ CosmeApp/Profile/MyProfileViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 import PGFramework
 // MARK: - Property
 class MyProfileViewController: BaseViewController {
+    @IBOutlet weak var mainView: MyProfileMainView!
 }
 // MARK: - Life cycle
 extension MyProfileViewController {
