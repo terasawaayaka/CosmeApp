88bbff7993119ef81c1893b13b54f7f31c8102d7
ASW-研修３ <training03@ASW-kenshuu3noMacBook-Air.local>
2020-05-18 15:43:03 +0900

    検索結果のところのcollectionView作った

---
+++ CosmeApp/Search/Search result/UserResultCell/SearchUserResultTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  SearchUserResultTableViewCell.swift
+//  CosmeApp
+//
+//  Created by ASW-研修３ on 2020/05/18.
+//  Copyright © 2020 ASW-研修２. All rights reserved.
+//
+
+import UIKit
+
+class SearchUserResultTableViewCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
