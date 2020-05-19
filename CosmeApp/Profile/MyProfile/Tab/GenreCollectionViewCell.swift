a3a57440a558d3d77ee05fb52bcfbc43df583fa6
norimatsushuusei <s.norimatsu@a-sw.jp>
2020-05-19 17:09:41 +0900

    fix profile

---
+++ CosmeApp/Profile/MyProfile/Tab/GenreCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  GenreCollectionViewCell.swift
+//  CosmeApp
+//
+//  Created by ASW-研修４ on 2020/05/19.
+//  Copyright © 2020 ASW-研修２. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+protocol GenreCollectionViewCellDelegate: NSObjectProtocol{
+}
+extension GenreCollectionViewCellDelegate {
+}
+// MARK: - Property
+class GenreCollectionViewCell: BaseCollectionViewCell {
+    weak var delegate: GenreCollectionViewCellDelegate? = nil
+}
+// MARK: - Life cycle
+extension GenreCollectionViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+// MARK: - Protocol
+extension GenreCollectionViewCell {
+}
+// MARK: - method
+extension GenreCollectionViewCell {
+}
--- CosmeApp/Profile/MyProfile/Tab/ScrollMainView.swift
+++ CosmeApp/Profile/MyProfile/Tab/ScrollMainView.swift
@@ -15,10 +15,13 @@ extension ScrollMainViewDelegate {
 // MARK: - Property
 class ScrollMainView: BaseView {
     weak var delegate: ScrollMainViewDelegate? = nil
+    //mainView
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var goodMainView: UIView!
     @IBOutlet weak var bookMarkMainView: UIView!
     @IBOutlet weak var genreMainView: UIView!
+    
+    //collectionView
     @IBOutlet weak var goodCollectionView: UICollectionView!
     @IBOutlet weak var collectionViewFlowLayout: UICollectionViewFlowLayout!
 }
@@ -29,7 +32,9 @@ extension ScrollMainView {
         setDelegate()
         
       //  collectionViewFlowLayout.estimatedItemSize = CGSize(width: goodCollectionView.frame.height, height: goodCollectionView.frame.width)
+        loadCollectionViewCellFromXib(collectionView: goodCollectionView, cellName: "BookmarkCollectionViewCell")
         loadCollectionViewCellFromXib(collectionView: goodCollectionView, cellName: "GoodCollectionViewCell")
+        loadCollectionViewCellFromXib(collectionView: goodCollectionView, cellName: "GenreCollectionViewCell")
     }
 }
 // MARK: - Protocol
@@ -38,11 +43,21 @@ extension ScrollMainView :UICollectionViewDataSource{
         return 30
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {   
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GoodCollectionViewCell", for: indexPath) as? GoodCollectionViewCell else {return UICollectionViewCell()}
-        return cell
-    }
-    
+        guard let secondCell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookmarkCollectionViewCell", for: indexPath) as? BookmarkCollectionViewCell else {return UICollectionViewCell()}
+        guard let thirdCell = collectionView.dequeueReusableCell(withReuseIdentifier: "GenreCollectionViewCell", for: indexPath) as? GenreCollectionViewCell else {return UICollectionViewCell()}
+        switch indexPath.row {
+        case 0:
+            return cell
+        case 3:
+            return secondCell
+        case 6:
+            return thirdCell
+        default:
+            return cell
+        }
+}
 }
 // MARK: - method
 extension ScrollMainView {
