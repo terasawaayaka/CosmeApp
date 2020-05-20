ccd2a10693041f8e4f37a8a456ca0442ee278faa
norimatsushuusei <s.norimatsu@a-sw.jp>
2020-05-20 11:03:06 +0900

    fix profile

--- CosmeApp/Profile/MyProfile/Tab/ScrollMainView.swift
+++ CosmeApp/Profile/MyProfile/Tab/ScrollMainView.swift
@@ -30,6 +30,8 @@ extension ScrollMainView {
         
       //  collectionViewFlowLayout.estimatedItemSize = CGSize(width: goodCollectionView.frame.height, height: goodCollectionView.frame.width)
         loadCollectionViewCellFromXib(collectionView: goodCollectionView, cellName: "GoodCollectionViewCell")
+        loadCollectionViewCellFromXib(collectionView: goodCollectionView, cellName: "BookmarkCollectionViewCell")
+        loadCollectionViewCellFromXib(collectionView: goodCollectionView, cellName: "GenreCollectionViewCell")
     }
 }
 // MARK: - Protocol
@@ -40,7 +42,18 @@ extension ScrollMainView :UICollectionViewDataSource{
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GoodCollectionViewCell", for: indexPath) as? GoodCollectionViewCell else {return UICollectionViewCell()}
-        return cell
+        guard let secondCell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookmarkCollectionViewCell", for: indexPath) as? BookmarkCollectionViewCell else {return UICollectionViewCell()}
+        guard let thirdCell = collectionView.dequeueReusableCell(withReuseIdentifier: "GenreCollectionViewCell", for: indexPath) as? GenreCollectionViewCell else {return UICollectionViewCell()}
+        switch indexPath.row {
+        case 0:
+            return cell
+        case 3:
+            return secondCell
+        case 6:
+            return thirdCell
+        default:
+            return cell
+        }
     }
     
 }
