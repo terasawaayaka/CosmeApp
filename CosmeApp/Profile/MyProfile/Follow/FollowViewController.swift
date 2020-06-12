c07aa34d26d470009d0f2ea01ebc57b7a072bcea
norimatsushuusei <s.norimatsu@a-sw.jp>
2020-06-12 22:34:30 +0900

    フォロワーリストを作成

--- CosmeApp/Profile/MyProfile/Follow/FollowViewController.swift
+++ CosmeApp/Profile/MyProfile/Follow/FollowViewController.swift
@@ -8,25 +8,96 @@
 
 import UIKit
 import PGFramework
+import FirebaseAuth
 // MARK: - Property
 class FollowViewController: BaseViewController {
     @IBOutlet weak var mainView: FollowMainView!
+    @IBOutlet weak var headerView: HeaderView!
+    
+    var userModel: UserModel = UserModel()
+    var userModels: [UserModel] = [UserModel]()
+    
+    var follow: Bool = false
+    var follower: Bool = false
 }
 // MARK: - Life cycle
 extension FollowViewController {
     override func loadView() {
         super.loadView()
+        setDelegate()
+        setHeaderView()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        getModel()
     }
 }
 // MARK: - Protocol
-extension FollowViewController {
+extension FollowViewController :FollowMainViewDelegate{
+    func didSelectRowAt(indexPath: IndexPath) {
+        let yourProfileViewController = YourProfileViewController()
+        yourProfileViewController.userModel = userModels[indexPath.row]
+        navigationController?.pushViewController(yourProfileViewController, animated: true)
+        animatorManager.navigationType = .slide_push
+    }
+}
+extension FollowViewController :HeaderViewDelegate{
+    func touchedLeftButton(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+        animatorManager.navigationType = .slide_pop
+    }
 }
 // MARK: - method
 extension FollowViewController {
+    func setDelegate() {
+        mainView.delegate = self
+        headerView.delegate = self
+    }
+    func setHeaderView() {
+        if follow {
+            headerView.setCenter(text: "フォロー", fontSize: 20, color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        }
+        if follower {
+            headerView.setCenter(text: "フォロワー", fontSize: 20, color: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+        }
+        headerView.setLeft(text: "戻る", fontSize: 20, color: #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1))
+    }
+    func getModel() {
+        if follow {
+        var followUsers = [UserModel]()
+        UserModel.readMe { (userModel) in
+            userModel.follow_users.forEach { (followUser) in
+                followUser.forEach { (key, val) in
+                    if val == true {
+                        UserModel.readAt(userId: key) { (userModel) in
+                            followUsers.append(userModel)
+                            self.mainView.getModel(userModels: followUsers)
+                            self.userModels = followUsers
+                        }
+                    }
+                }
+            }
+        }
+        }
+        if follower {
+            var followerUsers = [UserModel]()
+            UserModel.readMe { (userModel) in
+                userModel.follower_users.forEach { (followerUser) in
+                    followerUser.forEach { (key, val) in
+                        if val == true {
+                            UserModel.readAt(userId: key) { (userModel) in
+                                followerUsers.append(userModel)
+                                self.mainView.getModel(userModels: followerUsers)
+                                self.userModels = followerUsers
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
