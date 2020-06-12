050b966d1054881adef04d4c872314180f3c125f
norimatsushuusei <s.norimatsu@a-sw.jp>
2020-06-12 16:37:47 +0900

    フォロワーリストの作成

--- CosmeApp/Profile/MyProfile/Follow/FollowTableViewCell.swift
+++ CosmeApp/Profile/MyProfile/Follow/FollowTableViewCell.swift
@@ -15,11 +15,14 @@ extension FollowTableViewCellDelegate {
 // MARK: - Property
 class FollowTableViewCell: BaseTableViewCell {
     weak var delegate: FollowTableViewCellDelegate? = nil
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userIcon: UIImageView!
 }
 // MARK: - Life cycle
 extension FollowTableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
+//        setLayout()
     }
 }
 // MARK: - Protocol
@@ -27,4 +30,19 @@ extension FollowTableViewCell {
 }
 // MARK: - method
 extension FollowTableViewCell {
+//    func setLayout(){
+//        userIcon.layer.cornerRadius = userIcon.frame.width / 2
+//    }
+    func updateCell(userModel: UserModel) {
+        if userModel.nickname == "" {
+            userName.text = "メンバーがいません"
+        } else {
+            userName.text = userModel.nickname
+        }
+        if let photo_path = userModel.photo_path {
+            if let url = URL(string: photo_path) {
+                userIcon.af_setImage(withURL: url)
+            }
+        }
+    }
 }
