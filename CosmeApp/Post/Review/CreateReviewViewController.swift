58b2622a0e321226bbf85951cbb1309859aefc2e
terasawa <a.terasawa@a-sw.jp>
2020-06-17 15:35:45 +0900

    タブの切り替え

--- CosmeApp/Post/Review/CreateReviewViewController.swift
+++ CosmeApp/Post/Review/CreateReviewViewController.swift
@@ -132,6 +132,9 @@ extension CreateReviewViewController:HeaderViewDelegate {
             let timeLineViewController = TimeLineViewController()
             self.navigationController?.pushViewController(timeLineViewController, animated: true)
             self.animatorManager.navigationType = .pop
+            let newPostViewController = NewPostViewController()
+            self.navigationController?.pushViewController(newPostViewController, animated: true)
+            self.animatorManager.navigationType = .pop
         }
     }
 }
