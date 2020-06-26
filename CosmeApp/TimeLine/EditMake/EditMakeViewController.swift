d77227885478269630bf2ceb2abbcb0e1d55b5fb
UshigomeSaki <s.ushigome@a-sw.jp>
2020-06-26 15:22:08 +0900

    色の統一

--- CosmeApp/TimeLine/EditMake/EditMakeViewController.swift
+++ CosmeApp/TimeLine/EditMake/EditMakeViewController.swift
@@ -142,7 +142,7 @@ extension EditMakeViewController:EditMakeMainViewDelegate{
 extension EditMakeViewController {
     func setHeaderView(){
         headerView.setLeft(text: "キャンセル", fontSize: 16, color: #colorLiteral(red: 0.7404877639, green: 0.7449720201, blue: 1, alpha: 1))
-        headerView.setCenter(text: "メイク編集", fontSize: 20, color: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1))
+        headerView.setCenter(text: "メイク編集", fontSize: 18, color: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1))
         headerView.setRight(text: "完了", fontSize: 16, color: #colorLiteral(red: 1, green: 0.6230913235, blue: 0.7894609614, alpha: 1))
     }
     func setDelegate(){
